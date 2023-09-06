b4fe70d4a337fb9bef8fbb091bee042a99e9a2da
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-09-06 14:08:18 +0100

    root for try-radixUI

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,19 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+    <div className='border-solid border-2 border-black p-2'>
+      <i>PLAYGROUND</i>&nbsp;PLAYGROUND
+      <br/>
+      PLAYGROUND&nbsp;<i>PLAYGROUND</i>
+      <br/>
+      <i>PLAYGROUND</i>&nbsp;PLAYGROUND
+    </div>
+    <div>
+      <code>test-component-goes-here</code>
+    </div>
+    <div className='text-xs'>
+      <i>some footer (to keep the middle div in flex-col centered)</i>
+    </div>
     </main>
   )
 }
