51a672d22379c5bda5ee77e9c309f617808cd282
Caleb Durojaiye <dco2.caleb@gmail.com>
2023-09-06 15:31:46 +0100

    dialog with tailwindcss

--- app/dialog/page.tsx
+++ app/dialog/page.tsx
@@ -1,39 +1,55 @@
 "use client"
 import React from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
-import './styles.css';
 
 const DialogDemo = () => (
   <Dialog.Root>
     <Dialog.Trigger asChild>
-      <button className="Button violet">Edit profile</button>
+      <button className="text-violet11 shadow-blackA7 hover:bg-mauve3 inline-flex h-[35px] items-center justify-center rounded-[4px] bg-white px-[15px] font-medium leading-none shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none">
+        Edit profile
+      </button>
     </Dialog.Trigger>
     <Dialog.Portal>
-      <Dialog.Overlay className="DialogOverlay" />
-      <Dialog.Content className="DialogContent">
-        <Dialog.Title className="DialogTitle">Edit profile</Dialog.Title>
-        <Dialog.Description className="DialogDescription">
+      <Dialog.Overlay className="bg-blackA9 data-[state=open]:animate-overlayShow fixed inset-0" />
+      <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+        <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
+          Edit profile
+        </Dialog.Title>
+        <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
           Make changes to your profile here. Click save when you're done.
         </Dialog.Description>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="name">
+        <fieldset className="mb-[15px] flex items-center gap-5">
+          <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="name">
             Name
           </label>
-          <input className="Input" id="name" defaultValue="Pedro Duarte" />
+          <input
+            className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+            id="name"
+            defaultValue="Pedro Duarte"
+          />
         </fieldset>
-        <fieldset className="Fieldset">
-          <label className="Label" htmlFor="username">
+        <fieldset className="mb-[15px] flex items-center gap-5">
+          <label className="text-violet11 w-[90px] text-right text-[15px]" htmlFor="username">
             Username
           </label>
-          <input className="Input" id="username" defaultValue="@peduarte" />
+          <input
+            className="text-violet11 shadow-violet7 focus:shadow-violet8 inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+            id="username"
+            defaultValue="@peduarte"
+          />
         </fieldset>
-        <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}>
+        <div className="mt-[25px] flex justify-end">
           <Dialog.Close asChild>
-            <button className="Button green">Save changes</button>
+            <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
+              Save changes
+            </button>
           </Dialog.Close>
         </div>
         <Dialog.Close asChild>
-          <button className="IconButton" aria-label="Close">
+          <button
+            className="text-violet11 hover:bg-violet4 focus:shadow-violet7 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+            aria-label="Close"
+          >
             {/* <Cross2Icon /> */}
           </button>
         </Dialog.Close>
